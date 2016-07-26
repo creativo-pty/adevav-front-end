192c647587e5e6e20054e1e68e4ba8c3885edc7e
Christopher Duncan <cj.duncan.a@gmail.com>
2016-07-25 22:15:04 -0500

    Remove the Navigate directive

--- src/directives/index.js
+++ src/directives/index.js
@@ -3,7 +3,6 @@
 module.exports = (ngModule) => {
   // Load directives within this file by requiring it and passing the ngModule explicitly
   require('./carousel')(ngModule);
-  require('./navigate')(ngModule);
   require('./search-form')(ngModule);
   require('./social')(ngModule);
 };
