7755e28a8a364a34e2151fb132d06b06fa01ebc9
Christopher Duncan <cj.duncan.a@gmail.com>
2016-07-30 14:57:09 -0500

    Create a skeleton Contact Us page

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,5 +2,6 @@
 module.exports = (ngModule) => {
   // Load modules within this file by requiring it and passing the ngModule explicitly
   require('./aboutUs')(ngModule);
+  require('./contactUs')(ngModule);
   require('./home')(ngModule);
 };
