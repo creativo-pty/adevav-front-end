376cefbdf36b4fd929e5e7b5193373abcf1e1a2d
Christopher Duncan <cj.duncan.a@gmail.com>
2016-11-03 16:39:46 -0500

    Prepare the system to accept permissions

--- src/services/index.js
+++ src/services/index.js
@@ -3,6 +3,7 @@
 module.exports = (ngModule) => {
   // Load services within this file by requiring it and passing the ngModule explicitly
   require('./Auth.service')(ngModule);
+  require('./Permissions.service')(ngModule);
   require('./User.service')(ngModule);
   require('./Validator.service')(ngModule);
 };
